30c5002ff7368f2689234d8ae4c95ae13b5d4bb2
Wang chenyang <1546195620@qq.com>
2024-03-11 16:49:28 +0800

    微调避障算法

--- Controller.cpp
+++ Controller.cpp
@@ -26,51 +26,51 @@ void Controller::dispatch(int time) {
             int berth = assignBerth(&robots[i]);
             Item *best_item = nullptr;
             for (auto &item: game_map->items) {
-                if(!item.isValid(time)){
+                if (!item.isValid(time)) {
                     continue;
                 }
                 if (best_item == nullptr) {
                     best_item = &item;
                 } else {
                     if (best_item->value /
-                         (getdis(robots[i].pos, best_item->pos) + getdis(best_item->pos, berths[berth].pos)) <
-                         item.value/
-                          (getdis(robots[i].pos, item.pos) + getdis(item.pos, berths[berth].pos)) ){
-                      best_item=&item;
+                        (getdis(robots[i].pos, best_item->pos) + getdis(best_item->pos, berths[berth].pos)) <
+                        item.value /
+                        (getdis(robots[i].pos, item.pos) + getdis(item.pos, berths[berth].pos))) {
+                        best_item = &item;
                     }
                 }
             }
-            if(best_item == nullptr){
-                robots[i].berth_pos=berths[berth].pos;
+            if (best_item == nullptr) {
+                robots[i].berth_pos = berths[berth].pos;
                 continue;
             }
             best_item->setInvalid();
-            robots[i].task_type=TaskItem;
+            robots[i].task_type = TaskItem;
             robots[i].setRoute(best_item->pos);
-            robots[i].route=game_map->getRoute(robots[i].pos,best_item->pos);
-            robots[i].berth_pos=berths[berth].pos;
-            robots[i].item_pos=best_item->pos;
-        } else if(robots[i].task_type==TaskItem){
-            if(robots[i].route.empty()){
-                robots[i].task_type=TaskBerth;
-                robots[i].route=game_map->getRoute(robots[i].pos,robots[i].berth_pos);
-                for (auto& item:game_map->items) {
-                    if(item.pos==robots->item_pos){
+            robots[i].route = game_map->getRoute(robots[i].pos, best_item->pos);
+            robots[i].berth_pos = berths[berth].pos;
+            robots[i].item_pos = best_item->pos;
+        } else if (robots[i].task_type == TaskItem) {
+            if (robots[i].route.empty()) {
+                robots[i].task_type = TaskBerth;
+                robots[i].route = game_map->getRoute(robots[i].pos, robots[i].berth_pos);
+                for (auto &item: game_map->items) {
+                    if (item.pos == robots->item_pos) {
                         item.setInvalid();
                         break;
                     }
                 }
             }
-        } else{
-            if(robots[i].route.empty()){
-                robots[i].task_type=TaskIdle;
+        } else {
+            if (robots[i].route.empty()) {
+                robots[i].task_type = TaskIdle;
                 for (int j = 0; j < berth_num; ++j) {
-                    if(berths[j].pos==robots[i].berth_pos){
-                        for (auto& item:game_map->items) {
-                           if(item.pos==robots[i].item_pos){
-                               berths[j].addItem(&item);
-                               break;
-                           }
+                    if (berths[j].pos == robots[i].berth_pos) {
+                        for (auto &item: game_map->items) {
+                            if (item.pos == robots[i].item_pos) {
+                                berths[j].addItem(&item);
+                                break;
+                            }
                         }
                     }
                 }
@@ -78,41 +78,42 @@ void Controller::dispatch(int time) {
         }
     }
     for (int i = 0; i < berth_num; ++i) {
-        if(time>10000){
-            berths[i].ship->force_to_go= true;
+        if (time > 10000) {
+            berths[i].ship->force_to_go = true;
         }
-        if(berths[i].transport_time+time>=14000){
-            berths[i].ship->force_to_go= true;
+        if (berths[i].transport_time + time >= 14000) {
+            berths[i].ship->force_to_go = true;
         }
     }
     for (int i = 0; i < robot_num; ++i) {
-        for (int j = i+1; j < robot_num; ++j) {
-            if(i==j){
+        for (int j = 0; j < robot_num; ++j) {
+            if (i == j) {
                 continue;
             }
-            if (collision(&robots[i], &robots[j])){
-                    auto next1 = robots[i].route.top();
-                    auto next2 = robots[j].route.top();
-                    if (next1 == next2) {
-                        robots[i].route.push(robots[i].pos);
-                        continue;
-                    }
-                    if (next1 == robots[j].pos && next2 == robots[i].pos) {
-                        robots[i].route.push(robots[i].pre_pos.top());
-                        robots[i].pre_pos.pop();
-                        robots[i].route.push(robots[i].pre_pos.top());
-                        robots[i].pre_pos.pop();
-                    }
+            if (collision(&robots[i], &robots[j])) {
+                auto next1 = robots[i].route.top();
+                auto next2 = robots[j].route.top();
+                if (next1 == robots[j].pos && next2 == robots[i].pos) {
+                    robots[i].route.push(robots[i].pre_pos.top());
+                    robots[i].pre_pos.pop();
+                    robots[i].route.push(robots[i].pre_pos.top());
+                    robots[i].pre_pos.pop();
+                    continue;
+                }
+                if (next1 == next2) {
+                    robots[i].route.push(robots[i].pos);
+                }
+
             }
         }
     }
     //船舶需要船的引用
     for (int i = 0; i < ships_num; ++i) {
-        if(ships[i].status==1&&ships[i].target_id==-1){
-            for (auto berth:used) {
-                if(berths[berth.first].ship== nullptr||berths[berth.first].ship==&ships[i]){
-                    berths[berth.first].ship=&ships[i];
-                    ships[i].target_id=berth.first;
+        if (ships[i].status == 1 && ships[i].target_id == -1) {
+            for (auto berth: used) {
+                if (berths[berth.first].ship == nullptr || berths[berth.first].ship == &ships[i]) {
+                    berths[berth.first].ship = &ships[i];
+                    ships[i].target_id = berth.first;
                     break;
                 }
             }
@@ -121,9 +122,9 @@ void Controller::dispatch(int time) {
 }
 
 int Controller::assignBerth(Robot *robot) {
-    if(robot->berth_pos[0]!=-1){
+    if (robot->berth_pos[0] != -1) {
         for (int i = 0; i < berth_num; ++i) {
-            if(berths[i].pos==robot->berth_pos){
+            if (berths[i].pos == robot->berth_pos) {
                 return i;
             }
         }
@@ -152,7 +153,7 @@ int Controller::assignBerth(Robot *robot) {
 }
 
 bool Controller::collision(Robot *robot1, Robot *robot2) {
-    if(robot1->route.empty()||robot2->route.empty()){
+    if (robot1->route.empty() || robot2->route.empty()) {
         return false;
     }
     auto next1 = robot1->route.top();
@@ -167,10 +168,9 @@ bool Controller::collision(Robot *robot1, Robot *robot2) {
 }
 
 
-
 int Controller::getdis(Coord robot, Coord item) {
     auto route = game_map->getRoute(robot, item);
-    if(route.size()==0){
+    if (route.size() == 0) {
         return 1;
     }
     return route.size();
