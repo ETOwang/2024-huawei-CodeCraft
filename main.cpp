60297d37dbbdcebc0f2eb39015b2a7964c4ba3a3
XiEn <revir@qq.com>
2024-03-18 12:09:12 +0800

    add temporary changes

--- main.cpp
+++ main.cpp
@@ -107,7 +107,7 @@ void finish() {
  * 3.尽可能利用轮船运力
  */
 int main() {
-    srand(1);
+    srand(time(0));
     //流程：输入，调度，移动
     //freopen("D:\\24_huawei_com\\WindowsRelease\\record\\test.txt", "r", stdin);
     Init();
